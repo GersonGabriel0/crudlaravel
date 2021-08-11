649051b7d7c82afa966c7c732fd020de49d79e0f
GersonGabriel0 <gerson.ribeiro@aluno.unc.br>
2021-08-10 22:11:36 -0300

    criação do model, migration, controller, rota e views do Paciente

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //obtendos os dados do paciente
+        $pacientes = Paciente::all();
+        //chamando a tela e enviado o objeto pacientes como parametro 
+        return view ('pacientes.index', compact($pacientes));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //chamando a tela para o cadrastro de pacientes
+        return view ('pacientes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //criando regras para validacao
+        $validateData = $request->validate([
+            'nome'      =>      'required|max:35',
+            'genero'    =>      'required'
+        ]);
+        //executando o metodo para a gravacao do registro
+        $paciente = Paciente::create($validateData);
+        //redirecionando para a tela principal do moulo de paciente
+        return redirect ('/pacientes') -> with ('success', 'Dados adcionados com sucesso!');    
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
